5053b30df3681c5ba39bc83410333c9eff110db2
chenshenhai <cshenhai@gmail.com>
2021-06-08 13:42:22 +0800

    chore: example add data render

--- examples/dev/index.tsx
+++ examples/dev/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { TypeData } from '@idraw/types';
 import Studio from './../../src/components/studio';
-import data from './lib/data';
+import data from './libs/data';
 import './../../src/components/studio/style/index'
 
 ReactDOM.render(
