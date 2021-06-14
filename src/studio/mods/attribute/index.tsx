14dec7a7b6409aa07705432fe6e2000001060a38
chenshenhai <cshenhai@gmail.com>
2021-06-14 22:38:42 +0800

    feat: add studio render element data

--- src/studio/mods/attribute/index.tsx
+++ src/studio/mods/attribute/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StudioContext } from './../../context';
-import { Form, Input } from 'antd';
+import { Form, Input, Col, Row, } from 'antd';
 import { getElement } from './../../util/data'
 
 const { useContext, useState } = React;
@@ -27,20 +27,62 @@ const AttrForm: React.FC<AttrFormProps> = ({ onChange, fields }) => (
       onChange(allFields);
     }}
   >
-    <Form.Item
-      name="x"
-      label="X"
-      rules={[{ required: true, message: 'X is required!' }]}
-    >
-      <Input type="number" size="small" />
-    </Form.Item>
-    <Form.Item
-      name="y"
-      label="Y"
-      rules={[{ required: true, message: 'Y is required!' }]}
-    >
-      <Input type="number" size="small" />
-    </Form.Item>
+    <Row>
+      <Col span="12">
+        <Form.Item
+          name="x"
+          label="X"
+          rules={[{ required: true, message: 'X is required!' }]}
+        >
+          <Input type="number" size="small" />
+        </Form.Item>
+      </Col>
+      <Col span="12">
+        <Form.Item
+          name="y"
+          label="Y"
+          rules={[{ required: true, message: 'Y is required!' }]}
+        >
+          <Input type="number" size="small" />
+        </Form.Item>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col span="12">
+        <Form.Item
+          name="w"
+          label="W"
+          rules={[{ required: true, message: 'Width is required!' }]}
+        >
+          <Input type="number" size="small" />
+        </Form.Item>
+      </Col>
+      <Col span="12">
+        <Form.Item
+          name="h"
+          label="H"
+          rules={[{ required: true, message: 'Height is required!' }]}
+        >
+          <Input type="number" size="small" />
+        </Form.Item>
+      </Col>
+    </Row>
+    
+
+    <Row>
+      <Col span="24">
+        <Form.Item
+          name="angle"
+          label="Angle"
+          rules={[{ required: true, message: 'Angle is required!' }]}
+        >
+          <Input type="number" size="small" />
+        </Form.Item>
+      </Col>
+    </Row>
+    
+    
   </Form>
 );
 
@@ -52,6 +94,9 @@ export const Attribute = () => {
   const fields = [
     { name: ['x'], value: elem?.x || 0 },
     { name: ['y'], value: elem?.y || 0 },
+    { name: ['w'], value: elem?.w || 0 },
+    { name: ['h'], value: elem?.h || 0 },
+    { name: ['angle'], value: elem?.angle || 0 },
   ]
 
 
