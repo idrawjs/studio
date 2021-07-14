bd2589c1768c8b2c7b52a877e176f969f74c2ff4
chenshenhai <cshenhai@gmail.com>
2021-07-14 19:26:32 +0800

    feat: add header zoom action

--- src/studio/mods/header/index.tsx
+++ src/studio/mods/header/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { Layout, Select } from '../../../ui/antd';
 import classnames from 'classnames';
 import {
@@ -13,18 +13,72 @@ type TypeProps = {
   height: number;
 }
 
+const zoomData = [
+  {label: '25%', value: 0.25},
+  {label: '50%', value: 0.5},
+  {label: '75%', value: 0.75},
+  {label: '100%', value: 1},
+  {label: '125%', value: 1.25},
+  {label: '150%', value: 1.5},
+  {label: '200%', value: 2},
+  {label: '300%', value: 3},
+  {label: '400%', value: 4},
+]
+
+function getZoomInValue(value): number | null {
+  let val: number | null = null;
+  for (let i = 0; i < zoomData.length; i++) {
+    const item = zoomData[i];
+    if (item.value === value) {
+      if (zoomData[i + 1]) {
+        val = zoomData[i + 1].value
+      }
+      break;
+    }
+  }
+  return val;
+}
+
+function getZoomOutValue(value): number | null {
+  let val: number | null = null;
+  for (let i = 0; i < zoomData.length; i++) {
+    const item = zoomData[i];
+    if (item.value === value) {
+      if (zoomData[i - 1]) {
+        val = zoomData[i - 1].value
+      }
+      break;
+    }
+  }
+  return val;
+}
+
 export function StudioHeader(props: TypeProps) {
 
   const [scale, setScale] = useState<number>(1);
 
-  useEffect(() => {
-    
-  }, []);
+  console.log('rerender |scale = ', scale);
 
   const onChangeScale = useCallback((num) => {
     setScale(num);
     eventHub.trigger('studioScaleScreen', num);
-  }, []);
+  }, [scale]);
+
+  const onClickZoomIn = useCallback(() => {
+    const num = getZoomInValue(scale);
+    if (num > 0) {
+      setScale(num);
+      eventHub.trigger('studioScaleScreen', num);
+    }
+  }, [scale]);
+
+  const onClickZoomOut = useCallback(() => {
+    const num = getZoomOutValue(scale);
+    if (num > 0) {
+      setScale(num);
+      eventHub.trigger('studioScaleScreen', num);
+    }
+  }, [scale]);
 
   return (
     <Header className="idraw-studio-header"
@@ -35,23 +89,25 @@ export function StudioHeader(props: TypeProps) {
           style={{width: 100}}
           value={scale}
           onChange={onChangeScale}
-          options={[
-            {label: '25%', value: 0.25},
-            {label: '50%', value: 0.5},
-            {label: '75%', value: 0.75},
-            {label: '100%', value: 1},
-            {label: '125%', value: 1.25},
-            {label: '150%', value: 1.5},
-            {label: '200%', value: 2},
-            {label: '300%', value: 3},
-            {label: '400%', value: 4},
-          ]}/>
+          options={zoomData}/>
       </Box>
       <Box noBoarder={true}>
-        <ZoomInOutlined className="idraw-studio-header-icon" />
+        <ZoomInOutlined
+          onClick={onClickZoomIn}
+          className={classnames({
+            'idraw-studio-header-icon': true,
+            'icon-disable': getZoomInValue(scale) === null,
+          })} 
+        />
       </Box>
       <Box>
-        <ZoomOutOutlined className="idraw-studio-header-icon"/>
+        <ZoomOutOutlined
+          onClick={onClickZoomOut}
+          className={classnames({
+            'idraw-studio-header-icon': true,
+            'icon-disable': getZoomOutValue(scale) === null,
+          })} 
+        />
       </Box>
       <Box noBoarder={true}>
         <UndoOutlined className="idraw-studio-header-icon"/>
