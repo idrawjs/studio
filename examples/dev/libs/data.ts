832ba73b50229d2e0b4c96c1690c555d1dc4c626
chenshenhai <cshenhai@gmail.com>
2021-08-18 11:39:06 +0800

    chore: update example data

--- examples/dev/libs/data.ts
+++ examples/dev/libs/data.ts
@@ -1,3 +1,5 @@
+import { createLayerHTML } from './html';
+
 const data = {
   "bgColor": "#292929",
   "elements": [ 
@@ -10,48 +12,7 @@ const data = {
     desc: {
       width: 360,
       height: 250,
-      html: `
-      <style>
-      .layer-container {
-        position: relative;
-        height: 250px;
-        width: 360px;
-      }
-      .layer-content {
-        display: flex;
-        place-items: flex-end;
-        position: absolute;
-        left: calc(50% - 88px);
-        top: calc(50% - 86px);
-        width: 180px;
-        height: 320px;
-        background: #62278d;
-        background: -moz-linear-gradient(top,  #62278d 0%, #2cc09b 100%);
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#62278d), color-stop(100%,#2cc09b));
-        background: -webkit-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: -o-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: -ms-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: linear-gradient(to bottom,  #62278d 0%,#2cc09b 100%);
-        border: 1px #666666 solid;
-        transform: perspective(900px) translateX(15px) rotateX(45deg) rotate(10deg) skew(-15deg) translateZ(120px);;
-      }
-      .layer-text {
-        display: flex;
-        color: #ffffff;
-        font-size: 24px;
-        font-weight: bold;
-        border: 2px solid #ffffff;
-        margin: 10px;
-        padding: 4px;
-      }
-
-    </style>
-    <div class="layer-container">
-      <div class="layer-content ">
-        <span class="layer-text">HTML</span>
-      </div>
-    </div>
-      `,
+      html: createLayerHTML({ text: 'HTML' }),
     }
   },
   {
@@ -63,48 +24,7 @@ const data = {
     desc: {
       width: 360,
       height: 250,
-      html: `
-      <style>
-      .layer-container {
-        position: relative;
-        height: 250px;
-        width: 360px;
-      }
-      .layer-content {
-        display: flex;
-        place-items: flex-end;
-        position: absolute;
-        left: calc(50% - 88px);
-        top: calc(50% - 86px);
-        width: 180px;
-        height: 320px;
-        background: #62278d;
-        background: -moz-linear-gradient(top,  #62278d 0%, #2cc09b 100%);
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#62278d), color-stop(100%,#2cc09b));
-        background: -webkit-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: -o-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: -ms-linear-gradient(top,  #62278d 0%,#2cc09b 100%);
-        background: linear-gradient(to bottom,  #62278d 0%,#2cc09b 100%);
-        border: 1px #666666 solid;
-        transform: perspective(900px) translateX(15px) rotateX(45deg) rotate(10deg) skew(-15deg) translateZ(120px);;
-      }
-      .layer-text {
-        display: flex;
-        color: #ffffff;
-        font-size: 24px;
-        font-weight: bold;
-        border: 2px solid #ffffff;
-        margin: 10px;
-        padding: 4px;
-      }
-
-    </style>
-    <div class="layer-container">
-      <div class="layer-content ">
-        <span class="layer-text">SVG</span>
-      </div>
-    </div>
-      `,
+      html: createLayerHTML({ text: 'SVG' }),
     }
   }]
 }
---
+++ examples/dev/libs/html.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+type TypeProps = {
+  text: string;
+}
+
+function Layer(props: TypeProps) {
+  return (
+    <>
+      <div style={{
+         position: 'relative',
+         height: 250,
+         width: 360,
+      }}>
+        <div style={{
+          display: 'flex',
+          placeItems: 'flex-end',
+          position: 'absolute',
+          left: 'calc(50% - 88px)',
+          top: 'calc(50% - 86px)',
+          width: 180,
+          height: 320,
+          backgroundColor: '#62278d',
+          background: 'linear-gradient(to bottom,  #62278d 0%,#2cc09b 100%)',
+          border: '1px #666666 solid',
+          transform:' perspective(900px) translateX(15px) rotateX(45deg) rotate(10deg) skew(-15deg) translateZ(120px)',
+        }}>
+          <span style={{
+            display: 'flex',
+            color: '#ffffff',
+            fontSize: 24,
+            fontWeight: 'bold',
+            border: '2px solid #ffffff',
+            margin: 10,
+            padding: 4,
+          }}>{props.text}</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export function createLayerHTML(props = { text: '' }): string {
+  return renderToString(<Layer {...props}></Layer>)
+}
+
