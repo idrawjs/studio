7bc786d43e5f3b9c15dc41584989e19d81417a2e
chenshenhai <cshenhai@gmail.com>
2021-09-02 06:49:37 +0800

    fix: build scripts

--- scripts/utils/dark-vars.js
+++ scripts/utils/dark-vars.js
@@ -3,7 +3,7 @@ const path = require('path');
 const lessToJs = require('less-vars-to-js');
 
 const stylePath = path.join(__dirname, '..', '..', 'src', 'style');
-const darkLess = fs.readFileSync(path.join(stylePath, 'templates', 'dark.less'), 'utf8');
+const darkLess = fs.readFileSync(path.join(stylePath, 'themes', 'dark.less'), 'utf8');
 
 const darkPaletteLess = lessToJs(darkLess, {
   stripPrefix: true,
