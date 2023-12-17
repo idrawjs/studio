71cc70697fe72b9d2fd9b200a8080a22dfe641ae
chenshenhai <cshenhai@gmail.com>
2023-12-17 10:22:13 +0800

    feat: add mobile view limit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Studio } from '@idraw/studio';
-import data from './data'; 
+import data from './data';
 import './index.less';
 const dom = document.querySelector('#app') as HTMLDivElement;
 const root = createRoot(dom);
@@ -16,6 +16,55 @@ const App = () => {
       setHeight(window.innerHeight);
     });
   }, []);
+
+  if (width < 750) {
+    return (
+      <div
+        style={{
+          boxSizing: 'border-box',
+          height: '100%',
+          width: '100%',
+          background: '#1e1e1e',
+          color: '#ffffff',
+          fontSize: 20,
+          lineHeight: '32px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center'
+        }}
+      >
+        <div
+          style={{
+            padding: '0 10px',
+            boxSizing: 'border-box',
+            fontSize: 12
+          }}
+        >
+          {`${window.location.origin}${window.location.pathname}`}
+        </div>
+        <div
+          style={{
+            borderBottom: '1px solid #aaaaaa',
+            padding: 10,
+            boxSizing: 'border-box'
+          }}
+        >
+          Please browse the page on PC/desktop
+        </div>
+
+        <div
+          style={{
+            padding: 10,
+            boxSizing: 'border-box'
+          }}
+        >
+          请在PC/桌面端浏览页面
+        </div>
+      </div>
+    );
+  }
 
   // const style = { margin: 40 };
   // const width = 800;
@@ -54,5 +103,3 @@ const App = () => {
 };
 
 root.render(<App />);
-
- 
