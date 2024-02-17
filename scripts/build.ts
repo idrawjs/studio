66b916dcbb927ab494b85e03ba4562b2ce8560dd
chenshenhai <cshenhai@gmail.com>
2024-02-17 14:39:45 +0800

    chore: update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,5 @@
-import path from 'node:path'; 
-import { createServer } from 'vite';
-import pluginReact from '@vitejs/plugin-react'; 
+import path from 'node:path';
+import pluginReact from '@vitejs/plugin-react';
 import { build } from 'vite';
 import type { InlineConfig } from 'vite';
 
@@ -11,10 +10,30 @@ function resolve(...args) {
 const config: InlineConfig = {
   root: resolve(),
   base: './',
-  publicDir: resolve('public'), 
+  publicDir: resolve('public'),
   plugins: [pluginReact()],
-}
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'lib/react': ['react', 'react-dom'],
+          'lib/antd': ['antd'],
+          'lib/draw': ['idraw', '@idraw/studio']
+        },
+        entryFileNames: 'js/[name].[hash].js',
+        assetFileNames: '[ext]/[name].[hash].[ext]',
+        chunkFileNames: () => {
+          // const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
+          // const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+          return `js/[name].[hash].js`;
+        }
+      }
+    }
+  }
+};
 
-build(config).then(() => {
-  console.log('Build success!')
-}).catch(console.log)
+build(config)
+  .then(() => {
+    console.log('Build success!');
+  })
+  .catch(console.log);
