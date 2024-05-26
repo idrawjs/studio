a3f6587e85211e52020975cb93e6cdfc708ba10d
chenshenhai <cshenhai@gmail.com>
2024-05-26 15:09:01 +0800

    feat: update script of upgrading version

--- scripts/upgrade-version.ts
+++ scripts/upgrade-version.ts
@@ -1,9 +1,17 @@
+import process from 'node:process';
 import { readJSONFile, writeJSONFile } from './util/file';
 import { getRootPackageJSON, getAllSubPackageDirs } from './util/project';
 const pkg = getRootPackageJSON();
 const version = pkg.version;
 const idrawVersion = pkg.dependencies.idraw;
 const antdVersion = pkg.dependencies.antd;
+
+let workspacePrefix = 'workspace:';
+let forRelease = false;
+if (process.argv[2] === '--for-release') {
+  forRelease = true;
+  workspacePrefix = '';
+}
 
 async function run() {
   const pkgDirs = getAllSubPackageDirs();
@@ -15,33 +23,33 @@ async function run() {
   });
   for (const key in allPkgMap) {
     if (allPkgMap.hasOwnProperty(key)) {
-      console.log(
-        `Upgrade [${key}] from ${allPkgMap[key].json.version} to ${version}`
-      );
+      console.log(`Upgrade [${key}] from ${allPkgMap[key].json.version} to ${version}`);
       allPkgMap[key].json.version = version;
       if (allPkgMap[key]?.json?.dependencies) {
         for (const depName in allPkgMap[key].json.dependencies) {
           if (allPkgMap.hasOwnProperty(depName)) {
-            allPkgMap[key].json.dependencies[depName] = `^${version}`;
+            allPkgMap[key].json.dependencies[depName] = `${workspacePrefix}^${version}`;
           }
         }
       }
 
-      if (idrawVersion) {
-        if (allPkgMap[key].json?.dependencies?.['idraw']) {
-          allPkgMap[key].json.dependencies['idraw'] = `^${idrawVersion}`;
+      if (forRelease === false) {
+        if (idrawVersion) {
+          if (allPkgMap[key].json?.dependencies?.['idraw']) {
+            allPkgMap[key].json.dependencies['idraw'] = `^${idrawVersion}`;
+          }
+          if (allPkgMap[key].json.peerDependencies['idraw']) {
+            allPkgMap[key].json.peerDependencies['idraw'] = `^${idrawVersion}`;
+          }
         }
-        if (allPkgMap[key].json.peerDependencies['idraw']) {
-          allPkgMap[key].json.peerDependencies['idraw'] = `^${idrawVersion}`;
-        }
-      }
 
-      if (antdVersion) {
-        if (allPkgMap[key].json?.dependencies?.['antd']) {
-          allPkgMap[key].json.dependencies['antd'] = `^${antdVersion}`;
-        }
-        if (allPkgMap[key].json?.peerDependencies?.['antd']) {
-          allPkgMap[key].json.peerDependencies['antd'] = `^${antdVersion}`;
+        if (antdVersion) {
+          if (allPkgMap[key].json?.dependencies?.['antd']) {
+            allPkgMap[key].json.dependencies['antd'] = `^${antdVersion}`;
+          }
+          if (allPkgMap[key].json?.peerDependencies?.['antd']) {
+            allPkgMap[key].json.peerDependencies['antd'] = `^${antdVersion}`;
+          }
         }
       }
     }
